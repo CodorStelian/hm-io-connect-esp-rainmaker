9f0ca25edf21daf1c3ee06beb8a585c43da33169
Codor Stelian <codor.stelian@gmail.com>
2022-06-03 16:24:19 +0300

    app_wifi: Minor feature additions to provisioning workflow.

--- components/app_wifi/include/app_wifi.h
+++ components/app_wifi/include/app_wifi.h
@@ -6,8 +6,8 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #pragma once
-
 #include <esp_err.h>
+#include <esp_event.h>
 
 typedef esp_err_t (*app_wifi_connect_fn)();
 
@@ -18,15 +18,31 @@ struct app_wifi_config
     app_wifi_connect_fn wifi_connect;
 };
 
+// ESP RainMaker Event Base
+ESP_EVENT_DECLARE_BASE(APP_WIFI_EVENT);
+
+// App Wi-Fir Events
+typedef enum {
+    // QR code available for display. Associated data is the NULL terminated QR payload.
+    APP_WIFI_EVENT_QR_DISPLAY = 1,
+    // Provisioning timed out
+    APP_WIFI_EVENT_PROV_TIMEOUT,
+    // Provisioning has restarted due to failures (Invalid SSID/Passphrase)
+    APP_WIFI_EVENT_PROV_RESTART,
+} app_wifi_event_t;
+
 //Types of Proof of Possession
 typedef enum {
     //Use MAC address to generate PoP
     POP_TYPE_MAC,
     //Use random stream generated and stored in fctry partition during claiming process as PoP
-    POP_TYPE_RANDOM
+    POP_TYPE_RANDOM,
+	// Do not use any PoP.
+    // Use this option with caution. Consider using `CONFIG_APP_WIFI_PROV_TIMEOUT_PERIOD` with this.
+    POP_TYPE_NONE
 } app_wifi_pop_type_t;
 
 esp_err_t app_wifi_init(const struct app_wifi_config *config);
 esp_err_t app_wifi_start(app_wifi_pop_type_t pop_type);
-esp_err_t get_device_mac(char *mac, size_t max);
-esp_err_t get_device_pop(char *pop, size_t max, app_wifi_pop_type_t pop_type);
+esp_err_t get_dev_mac(char *mac);
+esp_err_t get_dev_pop(char *pop, app_wifi_pop_type_t pop_type);
